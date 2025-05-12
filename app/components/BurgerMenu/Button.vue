d2e47755e0809d71bbf55da37039769a79829f36
Nikita Antonov <nikita45454@gmail.com>
2025-05-12 12:59:35 +0500

    feat: menu - добавляет кнопку

---
+++ app/components/BurgerMenu/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="btn">
+    Меню
+  </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  .btn {
+    padding: 12px;
+    border-radius: 8px;
+    background: var(--color-white);
+    color: var(--color-base-01);
+    @include text-body-s-regular-01;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+</style>
--- app/components/LayoutParts/Header.vue
+++ app/components/LayoutParts/Header.vue
@@ -2,7 +2,10 @@
   <header class="header">
     <div class="wrapper">
       <div class="wrap">
-        <img width="95" height="32" src="~/assets/images/logo.svg" alt="Логотип MAAR" />
+        <NuxtLink class="logo-wrap" :to="{ name: 'index' }">
+          <img class="w-full" width="95" height="32" src="~/assets/images/logo.svg" alt="Логотип MAAR" />
+        </NuxtLink>
+        <BurgerMenuButton />
       </div>
     </div>
   </header>
@@ -16,5 +19,21 @@
   .header {
     padding: 12px 0;
     background: var(--color-base-01-75);
+  }
+
+  .logo-wrap {
+    width: 95px;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+
+  .wrap {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    column-gap: 40px;
   }
 </style>
