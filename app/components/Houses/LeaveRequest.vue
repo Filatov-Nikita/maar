765268ec784db34f533229410a49710e2d6583a9
Nikita Antonov <nikita45454@gmail.com>
2025-05-16 17:14:27 +0500

    feat: houses - добавляет LeaveRequest

---
+++ app/components/Houses/LeaveRequest.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <p class="title" v-html="title"></p>
+    <p class="text" v-html="text"></p>
+    <BaseButton class="w-full" @click="onClick">Оставить заявку</BaseButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    title: string,
+    text: string,
+    options?: Partial<FeedbackOptions>,
+  }>();
+
+  const feedback = useFeedbackStore();
+
+  function onClick() {
+    feedback.setValue(true, props.options);
+  }
+</script>
+
+<style scoped lang="scss">
+  .card {
+    border-radius: 16px;
+    padding: 16px;
+    background: var(--color-base-02);
+  }
+
+  .title {
+    margin-bottom: 8px;
+    @include text-body-l-regular-01;
+  }
+
+  .text {
+    margin-bottom: 16px;
+    color: var(--color-base-04);
+    @include text-body-s-regular-03;
+  }
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,13 @@
 <template>
   <HomeSectionMain />
   <div class="wrapper section-mt">
-    <Feedback />
+    <HousesLeaveRequest
+      title="Хочу это дом"
+      text="Оставьте заявку и наш специалист свяжется с вами в ближайшее время"
+    />
+    <Feedback
+      class="section-mt"
+    />
   </div>
 </template>
 
