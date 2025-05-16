80937eb090750fe130e55f369d2e2787fed6a3de
Nikita Antonov <nikita45454@gmail.com>
2025-05-16 21:06:19 +0500

    feat: добавляет PageSeoText

---
+++ app/components/Page/SeoText.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="block">
+    <h2 class="h2">{{ title }}</h2>
+    <div class="body" v-if="text" v-html="text"></div>
+    <div class="body" v-else>
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    title: string,
+    text?: string,
+  }>();
+</script>
+
+<style scoped lang="scss">
+  .block {
+    border-radius: 24px;
+    padding: 32px 16px;
+    background: var(--color-base-02);
+  }
+
+  .body {
+    margin-top: 16px;
+    color: var(--color-secondary-04);
+    @include text-body-s-regular-04;
+  }
+</style>
